f0214fb0fb3619bff014d43da295bdd0f41acdba
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2012-09-29 06:51:27 -0500

    remove extra space

--- src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageSenderRouteBuilder.java
+++ src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageSenderRouteBuilder.java
@@ -6,7 +6,6 @@ public class JMSMessageSenderRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-
 		from("timer://testMessageSenderTimer?fixedRate=true&period=500")
 				.bean(new JMSMessageBuilder()).log("publishing ${body}")
 				.to("activemq:topic:test.websocket.topic");
