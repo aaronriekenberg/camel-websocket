0c0cffb62c8067b6f9dafe617f3daa67d8e74d5a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2012-09-29 06:52:27 -0500

    counter shouldn't be static

--- src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageBuilder.java
+++ src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageBuilder.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class JMSMessageBuilder {
 
-	private static final AtomicInteger counter = new AtomicInteger(0);
+	private final AtomicInteger counter = new AtomicInteger(0);
 
 	public String buildMessage() {
 		return ("Message " + counter.getAndIncrement());
