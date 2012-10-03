58b82b357f8666293bea0764927fbf9148af0796
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2012-10-02 20:12:27 -0500

    Send XML message over JMS, marshal to JSON to send over websocket.

--- src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageBuilder.java
+++ src/main/java/org/aaron/camel/websocket/test/jmsroute/JMSMessageBuilder.java
@@ -1,13 +1,9 @@
 package org.aaron.camel.websocket.test.jmsroute;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class JMSMessageBuilder {
 
-	private final AtomicInteger counter = new AtomicInteger(0);
-
 	public String buildMessage() {
-		return ("Message " + counter.getAndIncrement());
+		return "<xmlMessage><stuff>things</stuff><timeMS>"
+				+ System.currentTimeMillis() + "</timeMS></xmlMessage>";
 	}
-
 }
--- src/main/java/org/aaron/camel/websocket/test/wsroute/WebsocketRouteBuilder.java
+++ src/main/java/org/aaron/camel/websocket/test/wsroute/WebsocketRouteBuilder.java
@@ -7,6 +7,8 @@ public class WebsocketRouteBuilder extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("activemq:topic:test.websocket.topic")
+				.marshal()
+				.xmljson()
 				.log("publishing ${body}")
 				.to("websocket://0.0.0.0:9090/camel-test?sendToAll=true&staticResources=classpath:webapp");
 	}
